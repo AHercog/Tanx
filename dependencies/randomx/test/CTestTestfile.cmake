0084ca489427aab582d6135b416466cae937f259
AHercog <anubarak123@gmail.com>
2018-12-27 08:35:49 +0100

    do some things

---
+++ dependencies/randomx/test/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/silvair/Projekty/Tanx/dependencies/random/test
+# Build directory: /home/silvair/Projekty/Tanx/dependencies/random/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(random_static_test "/home/silvair/Projekty/czolgi/dependencies/random/test/random_static_test")
+add_test(random_thread_local_test "/home/silvair/Projekty/czolgi/dependencies/random/test/random_thread_local_test")
+add_test(random_local_test "/home/silvair/Projekty/czolgi/dependencies/random/test/random_local_test")
